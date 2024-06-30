97f2e1f0af65768abc77416c0f356d152a3c3a17
Adi-Sin <adiaggarwal26@gmail.com>
2024-06-30 13:49:34 +0000

    feat: create daytona-web package

---
+++ packages/daytona-web/src/definitions/apis.ts
@@ -0,0 +1,51 @@
+import { OAuth2 } from "@backstage/core-app-api";
+import { ApiRef, BackstageIdentityApi, OpenIdConnectApi, ProfileInfoApi, SessionApi, 
+    createApiFactory, createApiRef, discoveryApiRef, oauthRequestApiRef, configApiRef } from "@backstage/core-plugin-api";
+
+/**
+ * Provides authentication towards Daytona APIs.
+ *
+ * @public
+ * @remarks
+ *
+ */
+export const daytonaAuthApiRef: ApiRef<
+    OpenIdConnectApi & ProfileInfoApi & BackstageIdentityApi & SessionApi
+> = createApiRef({
+  id: 'auth.daytona.oidc',
+});
+
+export const daytonaApiFactory = createApiFactory({
+api: daytonaAuthApiRef,
+deps: {
+    discoveryApi: discoveryApiRef,
+    oauthRequestApi: oauthRequestApiRef,
+    configApi: configApiRef,
+},
+factory: ({ discoveryApi, oauthRequestApi, configApi }) =>
+    OAuth2.create({
+        configApi,
+        discoveryApi,
+        oauthRequestApi,
+        provider: {
+            id: 'daytona',
+            title: 'Daytona OIDC Auth Provider',
+            icon: () => null,
+        },
+        environment: configApi.getOptionalString('auth.environment'),
+        defaultScopes: ['openid', 'profile', 'email'],
+        popupOptions: {
+            // optional, used to customize login in popup size
+            size: {
+            fullscreen: false,
+            },
+            /**
+             * or specify popup width and height
+             * size: {
+                 width: 1000,
+                height: 1000,
+            }
+            */
+        },
+    }),
+})
