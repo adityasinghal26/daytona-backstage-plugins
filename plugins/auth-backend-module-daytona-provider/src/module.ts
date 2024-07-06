578b0950f4d104ef327ffd6ad58b831a29d7c341
Adi-Sin <adiaggarwal26@gmail.com>
2024-07-06 11:07:17 +0000

    fix: add sign in resolver for auth module

--- plugins/auth-backend-module-daytona-provider/src/module.ts
+++ plugins/auth-backend-module-daytona-provider/src/module.ts
@@ -2,8 +2,9 @@ import {
   coreServices,
   createBackendModule,
 } from '@backstage/backend-plugin-api';
-import { authProvidersExtensionPoint, createOAuthProviderFactory } from '@backstage/plugin-auth-node';
+import { authProvidersExtensionPoint, commonSignInResolvers, createOAuthProviderFactory } from '@backstage/plugin-auth-node';
 import { oidcAuthenticator } from '@backstage/plugin-auth-backend-module-oidc-provider';
+import { stringifyEntityRef, DEFAULT_NAMESPACE } from '@backstage/catalog-model';
 
 export const authModuleDaytonaProvider = createBackendModule({
   pluginId: 'auth',
@@ -20,6 +21,39 @@ export const authModuleDaytonaProvider = createBackendModule({
           providerId: 'daytona',
           factory: createOAuthProviderFactory({
             authenticator: oidcAuthenticator,
+            signInResolverFactories: {
+              ...commonSignInResolvers
+            },
+            async signInResolver({ profile }, ctx) {
+              if (!profile.email) {
+                throw new Error(
+                  'Login failed, user profile does not contain an email',
+                );
+              }
+              // Split the email into the local part and the domain.
+              const [localPart, domain] = profile.email.split('@');
+            
+              // Next we verify the email domain. It is recommended to include this
+              // kind of check if you don't look up the user in an external service.
+              if (domain !== 'gmail.com') {
+                throw new Error(
+                  `Login failed, '${profile.email}' does not belong to the expected domain`,
+                );
+              }
+            
+              // By using `stringifyEntityRef` we ensure that the reference is formatted correctly
+              const userEntity = stringifyEntityRef({
+                kind: 'User',
+                name: localPart,
+                namespace: DEFAULT_NAMESPACE,
+              });
+              return ctx.issueToken({
+                claims: {
+                  sub: userEntity,
+                  ent: [userEntity],
+                },
+              });
+            }
           })
         })
       },
